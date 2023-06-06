212790b8f66b21a65da8dce37e3d47e0c1f7d4ec
jang ssau <seowoojang05@gmail.com>
2023-06-06 00:40:55 -0400

    added home and search html in templates, done redirecting from export, created mock database, done download csv file feature

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
-from extractors.wwr import extract_wwr_jobs
 from extractors.wic import extract_wic_jobs
+from extractors.wwr import extract_wwr_jobs
 from file import save_to_file
 
-#make CSV file
-keyword = input("What do you want to search for?")
-file_name = input("File name:")
+app = Flask("JobScrapper")
 
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-wic = extract_wic_jobs()
+db = {}
 
-jobs = indeed + wwr + wic
-save_to_file(file_name, jobs)
+@app.route("/")
+def home():
+  return render_template("home.html", name = "Tho")
+
+@app.route("/search")  
+def search():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    wic = extract_wic_jobs()
+    wwr = extract_wwr_jobs(keyword)
+    indeed = extract_indeed_jobs(keyword)
+    jobs = indeed + wwr + wic
+    db[keyword] = jobs
+  return render_template("search.html", keyword = keyword, jobs = jobs)
+
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True)
+    
 
 
-
-# from flask import Flask, render_template, request
-# from extractors.indeed import extract_indeed_jobs
-# from extractors.wic import extract_wic_jobs
-# from extractors.wwr import extract_wwr_jobs
-
-# app = Flask("JobScrapper")
-
-# @app.route("/")
-# def home():
-#   return render_template("home.html", name = "Tho")
-
-# @app.route("/search")  
-# def search():
-#   keyword = request.args.get("keyword")
-#   wic = extract_wic_jobs()
-#   wwr = extract_wwr_jobs(keyword)
-#   indeed = extract_indeed_jobs(keyword)
-#   jobs = indeed
-#   return render_template("search.html", keyword = keyword, jobs = jobs)
-
-# app.run("0.0.0.0")
+app.run("127.0.0.1")
 
